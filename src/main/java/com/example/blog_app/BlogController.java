02b181af8aaceaf925a6ea471e004d326aeb61d6
enokida <2026006@fsc-educ.com>
2026-06-05 13:49:37 +0900

    blog.html・post.html・about.htmlファイル作成

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,5 +1,14 @@
 package com.example.blog_app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class BlogController {
+    @GetMapping("/blog")
+    public String allBlog() {
+        return "blog";
+    }
     
 }
