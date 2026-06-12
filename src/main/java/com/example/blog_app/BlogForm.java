e985193065b98296abdb2cd79a9dd6a91b46a72d
enokida <2026006@fsc-educ.com>
2026-06-12 09:39:30 +0900

    エラー解決！

--- src/main/java/com/example/blog_app/BlogForm.java
+++ src/main/java/com/example/blog_app/BlogForm.java
@@ -1,16 +1,34 @@
 package com.example.blog_app;
 
 public class BlogForm {
+    private String author;
     private String title;
     private String content;
 
     // 引数なしのコンストラクタ（Spring Bootが自動で使うために必要）
-    public BlogForm() {}
+    public BlogForm() {
+    }
 
     // ゲッターとセッター
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() {
+        return author;
+    }
 
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title; 
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content; 
+    }
+    public void setContent(String content) {
+        this.content = content; 
+    }
 }
